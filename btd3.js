f855ed82ae47f8ee1b5656cf218ae403cc7df68f
Cesar Aguirre <87144162+cesheep@users.noreply.github.com>
2022-05-25 10:01:33 -0500

    Update btd3.js

--- btd3.js
+++ btd3.js
@@ -126,7 +126,7 @@ async function getGnana(){
   document.getElementById('gnana').innerHTML = " GNANA IN BAG = "+poolGnana.slice(0,-18);
   const bananaCalc = parseInt(poolGnana);
   const bananaBal = await (bananaCalc*1.7);
-  strBal = String.valueOf(bananaBal);
+  Integer strBal = new Integer(bananaBal);
   console.log(strBal);
   //const rwdCalc = gnanaRwd*1.7;
   //const rwdBalance = await rwdCalc;

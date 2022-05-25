c84ee2989a5b3d6e73fb2c6555f9c88ebc238df4
Cesar Aguirre <87144162+cesheep@users.noreply.github.com>
2022-05-25 14:06:23 -0500

    Update and rename a30.js to a31.js

--- a31.js
+++ a31.js
@@ -104,7 +104,7 @@ const mainstUsdPrice = await mainstPrice.usdPrice;
   const txn =  new web3.eth.Contract(mainstABIParse,mainstContract);
   const tokenInfo =  await txn.methods.balanceOf(userAddress).call({from: window.userAddress});
   const mainstH = await tokenInfo;
-  const balanceDisplay = BigInt(parseInt(mainstH));
+  const balanceDisplay = (parseInt(mainstH)).toFixed(2);
  
 
 //PriceMath

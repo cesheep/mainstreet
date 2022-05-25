49eea716a7fea9f86b760cede00d897d35ed9827
Cesar Aguirre <87144162+cesheep@users.noreply.github.com>
2022-05-25 13:19:05 -0500

    Update and rename a21.js to a22.js

--- a22.js
+++ a22.js
@@ -136,7 +136,7 @@ async function getGnana(){
   const gnanaTxn =  new web3.eth.Contract(gnanaParse,GNANAContract);
   const gnanaBalance =  await gnanaTxn.methods.userInfo(bBagAd).call({from: window.userAddress});
   const poolGnana = await gnanaBalance.amount;
-  document.getElementById('gnana').innerHTML = " GNANA IN BAG = "+poolGnana.slice(0,-18);
+  document.getElementById('gnana').innerHTML = " GNANA IN BAG = "+poolGnana.slice(0,-18)+"."+poolGnana.slice(0,18);
 
 //BananaPrice
   const bananaOptions = {address: bananaToken, chain: "bsc",};
@@ -152,7 +152,7 @@ async function getGnana(){
   const splitTxn =  new web3.eth.Contract(splitParse,splitContract);
   const splitBalance =  await splitTxn.methods.balanceOf(bBagAd).call({from: window.userAddress});
   const poolSplit = await splitBalance;
-  document.getElementById('banana').innerHTML = " BANANA IN BAG = "+poolSplit.slice(0,-18);
+  document.getElementById('banana').innerHTML = " BANANA IN BAG = "+poolSplit.slice(0,-18)+"."+poolSplit.slice(18,2);
 
 //Math2Price
   bananaCv = parseInt(poolSplit,0);

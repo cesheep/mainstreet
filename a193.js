d7ce415088bc172b2fa6ae1e24b2b9b913e766d5
Cesar Aguirre <87144162+cesheep@users.noreply.github.com>
2022-05-26 16:14:13 -0500

    Update and rename a192.js to a193.js

--- a193.js
+++ a193.js
@@ -132,10 +132,10 @@ const bMath = (((BigNumber(mainstH)).toFormat(2)).toString());
 //MainstPriceMath
   hodl = parseInt(mainstH,0);
   usprice = parseInt(mainstUsdPrice,0);
-  hodlMath = (BigNumber(hodl*usprice))/DivBase;
+  hodlMath = (BigNumber(hodl*usprice))/DivBase2;
   hodlBalance = ((((BigNumber(hodlMath))/DivBase).toFixed(2)));
   document.getElementById('MainstBalance').innerHTML = bMath.slice(0,-15)+"."+bMath.slice(18,20);
-  console.log("$"+hodlBalance);
+  console.log("$"+hodlMath);
 }
 //MM Data
 async function getMonkeysData() {

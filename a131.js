4fc0b73e7a0424d5a2908b70df30d16a513d3774
Cesar Aguirre <87144162+cesheep@users.noreply.github.com>
2022-05-26 09:38:30 -0500

    Update and rename a130.js to a131.js

--- a131.js
+++ a131.js
@@ -81,7 +81,7 @@ function logOut() {
     }
 
 async function checkLogged(){
-  if(window.address){       
+  if(window.userAddress){       
     window.web3 = new Web3(window.ethereum);
     document.getElementById("address").innerText = window.userAddress;
     document.getElementById('btn-login').style.display = 'none';

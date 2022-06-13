a66dd0f7899e63ee081cdbba22bc047762404bbc
Cesar Aguirre <87144162+cesheep@users.noreply.github.com>
2022-06-13 00:45:14 -0500

    Update and rename a369.js to a370.js

--- a370.js
+++ a370.js
@@ -145,7 +145,7 @@ priceFix = parseFloat(mainstPrice,0);
 //GeckoBanana
 var BananaGecko = await $.getJSON(geckoBanana);
 var bnanaJson = await JSON.stringify(BananaGecko);
-bananaPrice = (BigNumber(bnanaJson.slice(53,63))).toFixed();
+bananaPrice = (BigNumber(bnanaJson.slice(53,61))).toFixed();
 console.log(bnanaJson);
 console.log(bananaPrice);
 bNanaPriceFix = parseFloat(bananaPrice,0);

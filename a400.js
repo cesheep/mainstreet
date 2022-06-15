c626a554c786ee3d4fdeb2294630a998a0ebb81c
Cesar Aguirre <87144162+cesheep@users.noreply.github.com>
2022-06-15 12:09:29 -0500

    Update and rename a399.js to a400.js

--- a400.js
+++ a400.js
@@ -205,9 +205,11 @@ document.getElementById('banana').innerHTML = BPFormat;
   toClaimB = await claimTXN.methods.mainstToDistribute().call({from: window.userAddress});
   claimBalance = await toClaimB;
   claimDisplay = (BigNumber(claimBalance))/DivFix9;
+
   claimVsPrice = (BigNumber(claimDisplay*window.mainstPrice)).toFixed(2);
   document.getElementById('bonusReward').innerHTML = "$"+claimVsPrice+ " per Monkey";
-
+  console.log(claimDisplay);
+  console.log(claimVsPrice);
 
 }
 

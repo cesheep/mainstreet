fcc8f17943208ce3a8ff7972a1e9d6d8a1d34428
Cesar Aguirre <87144162+cesheep@users.noreply.github.com>
2022-06-15 12:16:16 -0500

    Update and rename a405.js to a406.js

--- a406.js
+++ a406.js
@@ -205,7 +205,7 @@ document.getElementById('banana').innerHTML = BPFormat;
   toClaimB = await claimTXN.methods.mainstToDistribute().call({from: window.userAddress});
   claimBalance = await toClaimB;
   claimDisplay = (BigNumber(claimBalance))/DivFix9;
-  claimVsPrice = (claimDisplay*window.mainstPrice);
+  claimVsPrice = (claimDisplay*window.priceFix);
   document.getElementById('bonusReward').innerHTML = "$"+claimVsPrice+ " per Monkey";
   console.log(claimDisplay);
   console.log(claimVsPrice);

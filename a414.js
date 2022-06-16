b8198af7e50a3c934802aa97db288d6447306201
Cesar Aguirre <87144162+cesheep@users.noreply.github.com>
2022-06-15 23:34:49 -0500

    Update and rename a413.js to a414.js

--- a414.js
+++ a414.js
@@ -205,7 +205,7 @@ document.getElementById('banana').innerHTML = BPFormat;
   toClaimB = await claimTXN.methods.mainstToDistribute().call({from: window.userAddress});
   claimBalance = await toClaimB;
   claimDisplay = (BigNumber(claimBalance))/DivFix9;
-  claimVsPrice = (((BigNumber(claimDisplay)*window.mainstPrice))/window.realSupply).toFixed(2);
+  claimVsPrice = (((BigNumber(claimDisplay)*window.mainstPrice))/window.realSupply);
   document.getElementById('bonusReward').innerHTML = "$"+claimVsPrice+ " per Monkey";
   console.log(window.mainstPrice);
   console.log(claimDisplay);

f81e298b6ae47dd2bd0f63b4a5303d51bc0516e9
Cesar Aguirre <87144162+cesheep@users.noreply.github.com>
2022-09-14 12:09:47 -0500

    Update and rename a510.js to a511.js

--- a511.js
+++ a511.js
@@ -216,11 +216,11 @@ async function getGnana(){
   document.getElementById('bananabag').innerHTML = " $"+window.bagFormat;
   document.getElementById('bonusReward').innerHTML = "$"+window.claimBalance+ " per Monkey";
 }*/
-
-document.getElementById("claimButton").addEventListener("click", claimBag(){
-                                                            const rewardsTXN = new web3.eth.Contract(claimABI,claimContract);
+var claimBag(){
+    const rewardsTXN = new web3.eth.Contract(claimABI,claimContract);
     rewardClaim =  await rewardsTXN.methods.claimMainst().send({from: window.userAddress});
-                                                        });
+}
+document.getElementById("claimButton").addEventListener("click", claimBag());
 
 
 

fbde874a7ffe6b5eee60f735ec8d9dccce1fdfcb
Cesar Aguirre <87144162+cesheep@users.noreply.github.com>
2023-03-07 10:36:56 -0600

    Update and rename 20stats.js to 21stats.js

--- 21stats.js
+++ 21stats.js
@@ -6,11 +6,11 @@ const geckoMainst= "https://api.coingecko.com/api/v3/simple/price?ids=buymainstr
 async function MainstCall(){
   const response = await fetch(geckoMainst);
   const data =  await response.json();
-  //var {usdMainst, usdMktCap, usdVol, usd24} = data;
-  var {"buymainstreet": {usd} } = data.usd;
-  var usdMktCap = data.usd_market_cap;
-  var usdVol = data.usd_24h_vol;
-  var usd24 = data.usd_24h_change;
+  var {usdMainst, usdMktCap, usdVol, usd24} = data.buymainstreet;
+  //var {"buymainstreet": {usd} } = data.usd;
+  //var usdMktCap = data.usd_market_cap;
+  //var usdVol = data.usd_24h_vol;
+  //var usd24 = data.usd_24h_change;
   //mainstPrice = BigNumber(usdMainst).toFixed();
   document.getElementById('usdMainst').innerHTML = usdMainst;
   document.getElementById('usdMktCap').innerHTML = usdMktCap;

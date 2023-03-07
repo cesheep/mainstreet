195c0d5f2a89ddfb8a59b6c9b579b77439154a57
Cesar Aguirre <87144162+cesheep@users.noreply.github.com>
2023-03-07 09:29:55 -0600

    Update and rename 2stats.js to 3stats.js

--- 3stats.js
+++ 3stats.js
@@ -3,12 +3,12 @@
 const geckoMainst= "https://api.coingecko.com/api/v3/simple/price?ids=buymainstreet&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true&precision=18"
 
 //GeckoMainst
+async function MainstCall(){
   var MainstGecko = await $.getJSON(geckoMainst);
   var {"0x8fc1a944c149762b6b578a06c0de2abd6b7d2b89": {usd} } = MainstGecko;
   mainstPrice = BigNumber(usd).toFixed();
-
-
-function MainstCall(){document.getElementById('mainstGecko').innerHTML = mainstPrice;}
+  document.getElementById('mainstGecko').innerHTML = mainstPrice;
+}
 
 
 

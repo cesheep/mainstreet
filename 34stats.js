4997c7eac33d5adaed8e3ef28a96479d0022c44f
Cesar Aguirre <87144162+cesheep@users.noreply.github.com>
2023-03-07 13:34:22 -0600

    Update and rename 33stats.js to 34stats.js

---
+++ 34stats.js
@@ -0,0 +1,59 @@
+
+//Mainst Coingecko Request
+const geckoMainst= "https://api.coingecko.com/api/v3/simple/price?ids=buymainstreet&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&precision=18";
+
+//GeckoMainst
+async function MainstCall(){
+
+ var usdMainst = await $.getJSON(geckoMainst);
+ var {"buymainstreet": {usd} } = usdMainst;
+ mainstDisplay = BigNumber(usd).toFixed(); 
+ 
+ var usdMktCap = await $.getJSON(geckoMainst);
+ var {"buymainstreet": {usd_market_cap} } = usdMktCap;
+ mktDisplay = JSON.parse(usd_market_cap).toFixed(2);
+ 
+ var usdVol = await $.getJSON(geckoMainst);
+ var {"buymainstreet": {usd_24h_vol} } = usdVol;
+ volDisplay = JSON.parse(usd_24h_vol).toFixed(2);
+ 
+  document.getElementById('usdMainst').innerHTML = "$"+mainstDisplay;
+  document.getElementById('usdMktCap').innerHTML = "$"+mktDisplay;
+  document.getElementById('usdVol').innerHTML = "$"+volDisplay;
+}
+
+const currencyEl_one = document.getElementById('currency-one');
+const currencyEl_two = document.getElementById('currency-two');
+const amountEl_one = document.getElementById('amount-one');
+const amountEl_two = document.getElementById('amount-two');
+
+const rateEl = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+// Fetch exchange rates and update the dome
+function calculate() {
+  const currency_one = currencyEl_one.value;
+  const currency_two = currencyEl_two.value;
+      amountEl_two.value = (amountEl_one.value * MainstDisplay).toFixed(2);
+    });
+
+
+// Event Listeners
+currencyEl_one.addEventListener('change', calculate);
+amountEl_one.addEventListener('input', calculate);
+currencyEl_two.addEventListener('change', calculate);
+amountEl_two.addEventListener('input', calculate);
+swap.addEventListener('click', () => {
+  const temp = currencyEl_one.value;
+  currencyEl_one.value = currencyEl_two.value;
+  currencyEl_two.value = temp;
+  calculate();
+});
+
+calculate();
+
+
+//--------------------------------------ONLOAD
+window.onload = async () =>{
+ MainstCall();
+}

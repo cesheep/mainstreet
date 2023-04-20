e98952c8067c912b3f3d3cc2d25043213afb6589
Cesar Aguirre <87144162+cesheep@users.noreply.github.com>
2023-04-19 19:26:30 -0600

    Update 89st.js

--- 89st.js
+++ 89st.js
@@ -60,7 +60,7 @@ window.web3 = new Web3(window.ethereum);
 }// End
 
 async function loginMetamask(){
-                       ethereum.request({method: 'eth_requestAccounts'})
+                       ethereum.request({method: 'eth_requestAccounts'});
  
         /* if (window.web3) {
                 const selectedAccount = await window.ethereum.request({method: "eth_requestAccounts",}).then((accounts) => accounts[0])
